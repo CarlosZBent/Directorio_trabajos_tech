c68de3416dd8e2161b21fe7170ae990596ab5689
carloszbent <bentsoft365@gmail.com>
2023-01-28 14:57:07 -0500

    made ItemsContainer a component instead of HTML markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import Config from '../platforms.config.json'
+import ItemsContainer from './ItemsContainer.svelte';
 import Item from './Item.svelte';
 import InfoWidget from './InfoWidget.svelte';
 import {iterateJSONData} from '../helpers.js'
@@ -14,9 +15,7 @@ let freelancingInfo = iterateJSONData(Config.freelancing)
 <hr class="border-emerald-400 w-[75%] m-auto" />
 
 <div class="flex flex-wrap justify-evenly mx-auto my-5 w-[75%]">
-    <div class="items-container w-[80%] min-w-fit md:w-[45%] my-2">
-       <h2 class="items-container-header"> Plataformas para trabajo </h2>
-       <hr class="border-emerald-400 w-[85%] mx-auto my-2">
+    <ItemsContainer title='Plataformas para trabajo' >
         {#each workInfo as data}
             <span class="item text-justify w-[90%]">
                 <Item
@@ -25,10 +24,8 @@ let freelancingInfo = iterateJSONData(Config.freelancing)
             </span>
             <hr>
         {/each}
-    </div>
-    <div class="items-container w-[80%] min-w-fit md:w-[45%] my-2">
-       <h2 class="items-container-header"> Plataformas para freelancing </h2>
-       <hr class="border-emerald-400 w-[85%] mx-auto my-2">
+    </ItemsContainer>
+    <ItemsContainer title='Plataformas para freelancing' >
        {#each freelancingInfo as data}
         <span class="item text-justify w-[90%]">
                 <Item
@@ -37,8 +34,9 @@ let freelancingInfo = iterateJSONData(Config.freelancing)
             </span>
             <hr>
         {/each}
-    </div>
+    </ItemsContainer>
 </div>
+
 <div class="infoContainer m-auto w-[75%]">
     <InfoWidget />
 </div>
@@ -47,19 +45,6 @@ let freelancingInfo = iterateJSONData(Config.freelancing)
     :global(html) {
         background-color: theme(colors.gray.100);
     }
-    .items-container {
-        /* max-width: 55%;
-        min-width: fit-content; */
-        height: fit-content;
-        min-height: 80px;
-        border: 1px #cacaca solid;
-        border-radius: 5px;
-        padding: 1em;
-    }
-        .items-container-header {
-            text-align: center;
-
-        }
     .infoContainer {
         border: 1px #cacaca solid;
         border-radius: 5px;
---
+++ src/routes/ItemsContainer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let title: string
+</script>
+
+<div class="items-container overflow-x-scroll w-full sm:w-[90%] sm:min-w-[40%] md:w-[45%] my-2">
+    <h2 class="items-container-header"> {title} </h2>
+    <hr class="border-emerald-400 w-[85%] mx-auto my-2">
+    <slot />
+</div>
+
+<style>
+.items-container {
+        /* max-width: 55%;
+        min-width: fit-content; */
+        height: fit-content;
+        min-height: 80px;
+        border: 1px #cacaca solid;
+        border-radius: 5px;
+        padding: 1em;
+    }
+        .items-container-header {
+            text-align: center;
+
+        }
+</style>
