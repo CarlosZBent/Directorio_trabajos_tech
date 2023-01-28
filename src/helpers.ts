4abc3bbf6c2940e46d866a9d42b186503b098367
carloszbent <bentsoft365@gmail.com>
2023-01-27 22:24:20 -0500

    fixed type checking errors on iterateJSONData function

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,19 @@
+interface ItemObject {
+    name: string,
+    /* link property has a type==any because if set to string it returns 
+    an error when being assigned to `val` inside the function,
+    maybe because of the way val is created iterating over an Object */
+    link: any
+}
+
 function iterateJSONData (obj: Object) {
     let data = []
     for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
-            let val = obj[key];
+            let val = obj[key as keyof typeof obj];
             /* new JSON object to store the data 
             for the {#each} block in the markup */
-            let tempDict = new Object()
+            let tempDict: ItemObject = {name:'', link:''}
             /* assigning the properties parsed from
             Config as values for the new Object*/
             tempDict.name = key
