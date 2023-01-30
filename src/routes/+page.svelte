54b3f82eb940237201056c5686ff172caa658ae4
carloszbent <bentsoft365@gmail.com>
2023-01-30 18:33:14 -0500

    changed top headers copy, changed h1 font to Cinzel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,8 @@ let workInfo = iterateJSONData(Config.work)
 let freelancingInfo = iterateJSONData(Config.freelancing)
 </script>
 
-<h1 class="text-center mx-auto my-5 text-2xl font-sans" use:mixpanelWebsiteVisit={mixpanel_id} > Directorio de trabajos tech de Gremio Creativo ✍ </h1>
+<h1 class="text-center mx-auto mb-5 mt-10 text-4xl font-Cinzel" use:mixpanelWebsiteVisit={mixpanel_id} > Directorio de trabajos tech </h1>
+<h2 class="text-center my-4 font-sans text-xl">¿Quieres trabajar en la industria tech? Tenemos decenas de plataformas para que encuentres tu próximo empleo</h2>
 <hr class="border-emerald-400 w-[75%] m-auto" />
 
 <div class="flex flex-wrap justify-evenly mx-auto my-5 w-[75%]">
