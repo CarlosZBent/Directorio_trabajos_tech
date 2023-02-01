3f3feb2e4cd6bd7e149401c3cea274a0ea52e7be
yossTheDev <yossthedev@gmail.com>
2023-02-01 09:24:46 -0500

    Add Dark Mode

--- src/routes/InfoWidget.svelte
+++ src/routes/InfoWidget.svelte
@@ -1,44 +1,92 @@
 <script>
-import github from "../brand-github.svg"
-import contribution from "../heart-handshake.svg"
-    
 </script>
 
-<div class="flex flex-row flex-auto gap-4">
-    <div class="flex flex-auto flex-col  gap-1 w-1/2 bg-white shadow p-3 rounded">
-        <div class="flex flex-auto mb-1 mx-auto">
-            <img class="h-6 my-auto" src={contribution} alt="contribution">
-            <h2 class="font-semibold text-xl my-auto">
-                Contribuye a la comunidad:
-            </h2>
-        </div>
-       
-        <p class="text-gray-600">
-            En el archivo <code>platforms.config.json</code> añade un nuevo par key-value en la categoría correspondiente a la plataforma. 
-            Si es una plataforma para freelancing: dentro de <code>freelancing</code> puedes añadirla de la siguiente forma <code>"nombre-de-plataforma": "URL"</code>.
-        </p>
-        
+<div class="flex flex-auto flex-col md:flex-row gap-4">
+  <div
+    class="flex flex-auto flex-col  gap-1 md:w-1/2 bg-white dark:bg-gray-800/20 shadow p-3 rounded-xl"
+  >
+    <div class="flex flex-auto mb-2 md:mx-auto dark:text-white gap-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="dark:stroke-white my-auto"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path
+          d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"
+        />
+        <path
+          d="M12 6l-3.293 3.293a1 1 0 0 0 0 1.414l.543 .543c.69 .69 1.81 .69 2.5 0l1 -1a3.182 3.182 0 0 1 4.5 0l2.25 2.25"
+        />
+        <path d="M12.5 15.5l2 2" />
+        <path d="M15 13l2 2" />
+      </svg>
+
+      <h2 class="font-semibold text-xl my-auto">Contribuye a la comunidad:</h2>
     </div>
-   
-    <div class="flex flex-auto flex-col gap-1 w-1/2 bg-white shadow p-3 rounded">
-        <div class="flex flex-auto mb-1 mx-auto">
-            <img class="h-6 my-auto" src={github} alt="contribution">
-            <h2 class="font-semibold text-xl my-auto">
-                Haz tuyo el directorio:
-            </h2>
-        </div>
-      
-        <p class="text-gray-600">
-            Esto apenas empieza, y no queremos que pare de crecer. Puedes hacer peticiones y recomendaciones de las siguientes formas:
-        </p>
-        <ol class="text-xs">
-            <li class="text-gray-600">1. Abre un <i>issue</i> en <a class="underline decoration-emerald-700" href="https://github.com/CarlosZBent/Directorio_trabajos_tech">este repositorio</a></li>
-            <li class="text-gray-600">2. Comenta lo que tienes en mente en <a class="underline decoration-emerald-700" href="https://t.me/CarlosZBent">este grupo de Telegram</a></li>
-        </ol>
+
+    <p class="text-gray-600">
+      En el archivo <code>platforms.config.json</code> añade un nuevo par
+      key-value en la categoría correspondiente a la plataforma. Si es una
+      plataforma para freelancing: dentro de <code>freelancing</code> puedes
+      añadirla de la siguiente forma <code>"nombre-de-plataforma": "URL"</code>.
+    </p>
+  </div>
+
+  <div
+    class="flex flex-auto flex-col gap-1 md:w-1/2 bg-white dark:bg-gray-800/20  shadow p-3 rounded-xl"
+  >
+    <div class="flex flex-auto mb-2 md:mx-auto dark:text-white gap-1">
+      <svg
+        class="dark:stroke-white my-auto"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path
+          d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+        />
+      </svg>
+
+      <h2 class="font-semibold text-xl my-auto">Haz tuyo el directorio:</h2>
     </div>
-    
+
+    <p class="text-gray-600">
+      Esto apenas empieza, y no queremos que pare de crecer. Puedes hacer
+      peticiones y recomendaciones de las siguientes formas:
+    </p>
+    <ol class="text-xs">
+      <li class="text-gray-600">
+        1. Abre un <i>issue</i> en
+        <a
+          class="underline decoration-emerald-700 text-gray-500"
+          href="https://github.com/CarlosZBent/Directorio_trabajos_tech"
+          >este repositorio</a
+        >
+      </li>
+      <li class="text-gray-600">
+        2. Comenta lo que tienes en mente en <a
+          class="underline decoration-emerald-700 text-gray-500"
+          href="https://t.me/CarlosZBent">este grupo de Telegram</a
+        >
+      </li>
+    </ol>
+  </div>
 </div>
 
 <style>
-
 </style>
--- src/routes/Item.svelte
+++ src/routes/Item.svelte
@@ -1,33 +1,29 @@
 <script lang="ts">
-    import copy from "../copy.svg"
-
-    export let name: string
-    export let link: string
-
-    async function copyTocClipboard(text:string){
-       await navigator.clipboard.writeText(text)
-    }
-
+  export let name: string;
+  export let link: string;
 </script>
 
+<div
+  class="flex flex-auto flex-row dark:bg-slate-800 bg-gray-100 shadow hover:shadow-xl hover:transition-all rounded-xl p-3"
+>
+  <a
+    href={link}
+    target="_blank rel=noreferrer"
+    class="flex flex-auto my-auto gap-2 overflow-hidden"
+  >
+    <img
+      class="h-8 my-auto dark:text-white bg-gray-100 dark:bg-slate-800 rounded"
+      src={"http://visiting-chocolate-meerkat.faviconkit.com/" +
+        link.replace("https://", "") +
+        "/48"}
+      alt="fav-icon"
+    />
 
-<div  class="flex flex-auto flex-row bg-white shadow hover:shadow-xl hover:transition-all rounded p-3">
-    <a  href={link} target="_blank rel=noreferrer" class="flex flex-auto my-auto gap-2 overflow-hidden">
-        <img class="h-8 my-auto" src={"http://visiting-chocolate-meerkat.faviconkit.com/" + link.replace("https://","") + "/48"} alt="fav-icon">
-
-        <h1 class="my-2 font-semibold" >
-            {name}
-        </h1>
-      
-    </a>
-    <button class="ml-auto p-2" on:click={() => copyTocClipboard(link)}>
-        <img alt="copy" src={copy}/>
-    </button>
-
-   
+    <h1 class="my-2 font-semibold ml-auto dark:text-white ">
+      {name}
+    </h1>
+  </a>
 </div>
 
-
 <style lang="postcss">
-
 </style>
